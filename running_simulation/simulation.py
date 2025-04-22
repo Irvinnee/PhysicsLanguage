44c995781e81ccbdb00a37f77c4160b1a74e0a16
Emilia Sarna <emilka.sarna@gmail.com>
2025-04-22 23:50:22 +0200

    wstępny zarys symulacji, trzeba poprawić przekazywanie danych między wątkami bo się kopiują a nie przekazywana jest referencja

---
+++ running_simulation/simulation.py
@@ -0,0 +1,65 @@
+import threading
+from running_simulation.engine import System, Particle
+# from running_simulation.graphics import GraphicsEngine
+from typing import Callable, Dict, List, Optional, Union
+import pygame
+from OpenGL.GL import *
+
+from running_simulation.grph2 import Graphics
+
+
+class Simulation:
+
+    system: System
+    graphics_engine: Graphics
+
+    particles_data: Dict[str, Particle]
+    time = 0.0
+    max_time = 0.0
+
+    # Lock do odbioru/zapisu danych
+    data_lock = threading.Lock()
+    time_lock = threading.Lock()
+
+    # Silnik symulacji
+    def simulation_engine(self):
+        while True:
+            with self.time_lock:
+                while abs(self.time*self.max_time - self.system.time) > 1:
+                    if self.time*self.max_time - self.system.time > 0:
+                        self.system.step(1)
+                    else:
+                        self.system.step(-1)
+
+                    with self.data_lock:
+                        self.particles_data = self.system.particles
+
+
+                print(self.time*self.max_time)
+
+
+    # Program graficzny
+    def graphics(self):
+        self.graphics_engine.run_simulation()
+
+    def run(self, system:System, max_time):
+        # Tworzenie wątków
+
+        self.system = system
+        self.particles_data = system.particles
+        self.max_time = max_time
+
+        self.graphics_engine = Graphics(self.particles_data, self.data_lock, self.time, self.time_lock)
+
+
+        engine_thread = threading.Thread(target=self.simulation_engine)
+
+
+        # Uruchomienie wątków
+        engine_thread.start()
+        # graphics_thread.start()
+        self.graphics()
+
+        # # Czekanie na zakończenie wątków
+        engine_thread.join(5)
+        # graphics_thread.join()
--- test.py
+++ test.py
@@ -4,8 +4,8 @@ from PhysicsParser import PhysicsParser
 from error_listener import ThrowingErrorListener
 from SymbolCollector import SymbolCollector
 from interpreter import Interpreter
-from engine import Particle, System
-
+from running_simulation.engine import Particle, System
+from running_simulation.simulation import Simulation
 
 with open("program.phys", encoding="utf-8") as f:
     code = f.read()
@@ -41,7 +41,23 @@ interpreter = Interpreter()
 interpreter.symbol_table = collector.symbol_table
 interpreter.visit(tree)
 
-if "cz1" in interpreter.variables and isinstance(interpreter.variables["cz1"], Particle):
-    dummy = System("global")
-    cz1 = interpreter.variables["cz1"]
-    dummy.add_particle("cz1", cz1)
+# if "cz1" in interpreter.variables and isinstance(interpreter.variables["cz1"], Particle):
+#     dummy = System("global")
+#     cz1 = interpreter.variables["cz1"]
+#     dummy.add_particle("cz1", cz1)
+#
+#     sim = Simulation()
+#     sim.run(dummy, 30)
+
+dummy = System("global")
+dummy.add_particle("cz1", Particle(4, [1,1,1]))
+dummy.add_particle("cz2", Particle(4, [0,0,0]))
+dummy.add_particle("cz3", Particle(4, [1,1,0]))
+dummy.add_particle("cz4", Particle(4, [0,1,1]))
+dummy.add_particle("cz5", Particle(4, [0,0,1]))
+dummy.add_particle("cz6", Particle(4, [1,0,1]))
+dummy.add_particle("cz7", Particle(4, [0,1,0]))
+dummy.add_particle("cz8", Particle(4, [1,0,0]))
+
+sim = Simulation()
+sim.run(dummy, 30)
