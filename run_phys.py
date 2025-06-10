cb59ca3744aafeb47b438d17cddfb9487d135443
antmarwas <wasikowska.antonina@gmail.com>
2025-06-10 15:29:50 +0200

    fixing run_phys

--- run_phys.py
+++ run_phys.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import glob
 from antlr4 import *
 from grammar.PhysicsLexer import PhysicsLexer
 from grammar.PhysicsParser import PhysicsParser
@@ -8,20 +9,18 @@ from interpreting.SymbolCollector import SymbolCollector
 from interpreting.interpreter import Interpreter
 from running_simulation.engine import Particle, System
 from running_simulation.simulation import Simulation
-from grammar.ScopeAndVisitor import PhysicsVisitor2
-import glob
-
 
 def run_phys_file(path, sim=False):
     with open(path, encoding="utf-8") as f:
         code = f.read()
 
+    # ANTLR: lexer + parser
     input_stream = InputStream(code)
     lexer = PhysicsLexer(input_stream)
     tokens = CommonTokenStream(lexer)
     parser = PhysicsParser(tokens)
 
-
+    # Obsługa błędów składniowych
     listener = ThrowingErrorListener()
     lexer.removeErrorListeners()
     parser.removeErrorListeners()
@@ -41,6 +40,7 @@ def run_phys_file(path, sim=False):
                 print("   ", err)
         exit(1)
 
+    # Pass 1: zbieranie symboli
     collector = SymbolCollector()
     walker = ParseTreeWalker()
     walker.walk(collector, tree)
@@ -51,52 +51,31 @@ def run_phys_file(path, sim=False):
             print("   ", err)
         exit(1)
 
+    # Pass 2: interpretacja
     interpreter = Interpreter()
     interpreter.symbol_table = collector.symbol_table
     interpreter.visit(tree)
 
-    simulate = Simulation()
+    # Zbieranie cząstek do systemu
     dummy = System("global")
-
     for name, value in interpreter.variables.items():
         if isinstance(value, Particle):
             dummy.add_particle(name, value)
 
+    delta = interpreter.variables.get("$DELTA", 1.0)
+    time_limit = interpreter.variables.get("$TIME", 10.0)
+    simulate = Simulation(list(dummy.particles.values()), delta)
+    simulate.global_laws = interpreter.global_laws
+
     if sim:
-        simulate.run(dummy, interpreter.variables["$TIME"], interpreter.variables["$DELTA"])
-
-def show_gui_before_simulation(path):
-    import sys
-    from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
-    import threading
-
-    class App(QWidget):
-        def __init__(self):
-            super().__init__()
-            self.setWindowTitle("Symulacja cząsteczek")
-            self.setGeometry(100, 100, 300, 150)
-
-            layout = QVBoxLayout()
-            self.label = QLabel(f"Plik: {path}")
-            self.button = QPushButton("Start symulacji")
-            self.button.clicked.connect(self.start_simulation)
-
-            layout.addWidget(self.label)
-            layout.addWidget(self.button)
-            self.setLayout(layout)
-
-        def start_simulation(self):
-            threading.Thread(target=lambda: run_phys_file(path, sim=False)).start()
-            self.close()
-
-    app = QApplication(sys.argv)
-    window = App()
-    window.show()
-    sys.exit(app.exec_())
-
+        simulate.run(dummy, time_limit, delta)
+    else:
+        simulate.simulate_to_time(time_limit)
+        print("Pozycje po symulacji:")
+        for p in simulate.particles:
+            print(p.position)
 
 if __name__ == "__main__":
-
     args = sys.argv[1:]
     simulation = "True" in args
 
@@ -107,7 +86,11 @@ if __name__ == "__main__":
         full_path = os.path.join(base_dir, phys_file) if not os.path.isabs(phys_file) else phys_file
         run_phys_file(full_path, sim=simulation)
     else:
-        files = sorted(glob.glob(os.path.join(base_dir, "przykladowe_programy/etap4", "*.phys")), key=os.path.getmtime, reverse=True)
+        files = sorted(
+            glob.glob(os.path.join(base_dir, "przykladowe_programy/etap4", "*.phys")),
+            key=os.path.getmtime,
+            reverse=True
+        )
         if not files:
             print("Nie znaleziono plików .phys.")
             exit(1)
--- running_simulation/simulation.py
+++ running_simulation/simulation.py
@@ -2,7 +2,6 @@ import threading
 from time import sleep
 
 from running_simulation.engine import System
-
 from running_simulation.visualisation_engine import Graphics
 
 
@@ -10,19 +9,12 @@ class Simulation:
 
     system: System
     graphics_engine: Graphics
-
-    # particles_data: Dict[str, Particle]
-    # time = 0.0
     max_time = 0.0
     delta = 1
-    #
-    # # Lock do odbioru/zapisu danych
-    # data_lock = threading.Lock()
-    # time_lock = threading.Lock()
 
     def __init__(self, particles, delta_time):
         import copy
-        self.particles = particles  # Dict[str, Particle]
+        self.particles = particles
         self.initial_state = copy.deepcopy(particles)
         self.delta_time = delta_time
         self.time = 0.0
@@ -31,8 +23,8 @@ class Simulation:
     def simulation_engine(self):
         while not self.stop_event.is_set():
             with self.graphics_engine.time_lock:
-                while abs(self.graphics_engine.sim_time*self.max_time - self.system.time) > self.delta:
-                    if self.graphics_engine.sim_time*self.max_time - self.system.time > 0:
+                while abs(self.graphics_engine.sim_time * self.max_time - self.system.time) > self.delta:
+                    if self.graphics_engine.sim_time * self.max_time - self.system.time > 0:
                         self.system.step(self.delta)
                     else:
                         self.system.step(-self.delta)
@@ -40,57 +32,41 @@ class Simulation:
                     with self.graphics_engine.data_lock:
                         self.graphics_engine.particles = self.system.particles
 
-
-                # print(self.graphics_engine.sim_time*self.max_time)
-
             sleep(0.01)
-
 
     # Program graficzny
     def graphics(self):
         self.graphics_engine.run_simulation()
 
-    def run(self, system:System, max_time, delta):
-        # Tworzenie wątków
-
+    def run(self, system: System, max_time, delta):
         self.system = system
         self.particles_data = system.particles
         self.max_time = max_time
         self.delta = delta
         self.stop_event = threading.Event()
 
-        # self.graphics_engine = Graphics(self.particles_data, self.data_lock, self.time, self.time_lock)
         self.graphics_engine = Graphics(self.system.particles, self.stop_event, self.system)
 
         engine_thread = threading.Thread(target=self.simulation_engine)
-
-
-        # Uruchomienie wątków
         engine_thread.start()
-        # graphics_thread.start()
+
         self.graphics()
         self.stop_event.set()
-
-        # # Czekanie na zakończenie wątków
         engine_thread.join(5)
-        # graphics_thread.join()
 
+    def reset(self):
+        import copy
+        self.particles = copy.deepcopy(self.initial_state)
+        self.time = 0.0
 
-        def reset(self):
-            import copy
-            self.particles = copy.deepcopy(self.initial_state)
-            self.time = 0.0
+    def step(self):
+        for p in self.particles:
+            for i in range(3):
+                p.position[i] += p.velocity[i] * self.delta_time
+        self.time += self.delta_time
 
-        def step(self):
-            # tutaj krok symulacji
-            for p in self.particles.values():
-                p.position += p.velocity * self.delta_time
-                # itd...
-            self.time += self.delta_time
-
-        def simulate_to_time(self, target_time: float):
-            self.reset()
-            steps = int(target_time / self.delta_time)
-            for _ in range(steps):
-                self.step()
-
+    def simulate_to_time(self, target_time: float):
+        self.reset()
+        steps = int(target_time / self.delta_time)
+        for _ in range(steps):
+            self.step()
